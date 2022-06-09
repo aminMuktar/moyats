a1ec974938d730223f07d4e470f951a1d5444899
amarmahdi <amarmehdi1112@gmail.com>
2022-06-09 16:40:19 +0300

    company management fixed

--- web/src/pages/dashboard/CompanyManagement.vue
+++ web/src/pages/dashboard/CompanyManagement.vue
@@ -1,25 +1,17 @@
 <template>
-  <div class="">
+  <div>
     <nav-header class="w-full fixed" />
-    <div class="w-full bg-gray-200 absolute flex justify-center">
-      <div class="w-4/6 grid grid-cols-3 items-center">
-        <div class="bg-white w-96 h-96 mt-28 rounded-lg shadow-md">
-          something
-        </div>
-        <div class="bg-white w-96 h-96 mt-28 rounded-lg shadow-md">
-          something
-        </div>
-        <div class="bg-white w-96 h-96 mt-28 rounded-lg shadow-md">
-          something
-        </div>
-        <div class="bg-white w-96 h-96 mt-28 rounded-lg shadow-md">
-          something
-        </div>
-        <div class="bg-white w-96 h-96 mt-28 rounded-lg shadow-md">
-          something
-        </div>
-        <div class="bg-white w-96 h-96 mt-28 rounded-lg shadow-md">
-          something
+    <div
+      class="w-full bg-gray-200 bottom-0 top-0 absolute flex justify-center overflow-auto"
+    >
+      <div class="w-4/6">
+        <div class="mt-28">Your Companies</div>
+        <div class="grid grid-cols-3 items-center">
+          <company-management-widget :url="'/dashboard'">
+            <template v-slot:header> something </template>
+            <template v-slot:subHeader> something </template>
+            <template v-slot:body> something </template>
+          </company-management-widget>
         </div>
       </div>
     </div>
@@ -28,10 +20,12 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import NavHeader from "../../components/NavHeader.vue";
+import CompanyManagementWidget from "../../components/widgets/CompanyManagementWidget.vue";
 
 export default defineComponent({
   components: {
     NavHeader,
+    CompanyManagementWidget,
   },
 });
 </script>
