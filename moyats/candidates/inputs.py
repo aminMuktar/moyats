9fab97f2c91726bba6cacbd84053078cf906f788
amenabe22 <bdere12345@gmail.com>
2022-06-11 21:19:05 +0300

    made some changes

---
+++ moyats/candidates/inputs.py
@@ -0,0 +1,29 @@
+import graphene
+
+
+class SocialMediaInput(graphene.InputObjectType):
+    link = graphene.String()
+
+
+class CandidateInput(graphene.InputObjectType):
+    firstName = graphene.String()
+    middleName = graphene.String()
+    lastName = graphene.String()
+    email = graphene.String()
+    organization = graphene.String()
+    phone = graphene.String()
+    # socialMediaType = graphene.String()
+    socialMedias = graphene.List(SocialMediaInput)
+    country = graphene.String()
+    city = graphene.String()
+    zipCode = graphene.Int()
+    source = graphene.String()
+    keySkills = graphene.String()
+    currentEmployeer = graphene.String()
+    dateAvailable = graphene.String()
+    currentPay = graphene.String()
+    desiredPay = graphene.String()
+    bestContactTime = graphene.String()
+    website = graphene.String()
+    qualifications = graphene.String()
+    attachments = graphene.String()
